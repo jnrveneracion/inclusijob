935878aab371a552406f85d3bd6a02143b0145ee
jnri <jrveneracion13@gmail.com>
2023-12-07 11:58:03 +0800

    compare lang ng email funct and db conn added

--- database/conn.php
+++ database/conn.php
@@ -1,3 +1,4 @@
 <?php 
      $conn = mysqli_connect("localhost", "root", "", "inclusijob_db");
+     // $conn = mysqli_connect("localhost", "u700918973_inclusijob", "bojKypbitdarmehru7", "u700918973_inclusijob");
 ?>
